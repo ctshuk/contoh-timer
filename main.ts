899c7814312b457e9e854af510abe2d3fd021e2e
ctshuk <57163286+ctshuk@users.noreply.github.com>
2023-10-09 16:39:24 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 input.onButtonPressed(Button.A, function () {
     start_time = input.runningTime()
+    mode = 1
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.B, function () {
+    mode = 0
+})
 let start_time = 0
-basic.showIcon(IconNames.Heart)
+let mode = 0
+mode = 0
 basic.forever(function () {
-    if (input.runningTime() - start_time >= 5000) {
+    if (mode == 1) {
+        if (input.runningTime() - start_time >= 5000) {
+            mode = 2
+        }
+    } else if (mode == 2) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         basic.showIcon(IconNames.Sad)
     }
 })
